271cf30e7bbd608fdffbb739d16737a84303a850
yashwanth2701 <yashwanthjakkampudi27@gmail.com>
2026-07-26 00:40:51 +0530

    Add files via upload

---
+++ assignments/reactjs/react2/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <About />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
